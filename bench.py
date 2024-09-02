7757a6e1bc912cf6f87dbc3af2a61eb399ff2058
no-cash-george <78621209+no-cash-george@users.noreply.github.com>
2024-09-02 17:33:34 +0300

    Update bench.py

--- bench.py
+++ bench.py
@@ -1,13 +1,16 @@
 import random 
 import time
 
+n = int(input("How many passes do you want me to perform? : \n"))
 start = time.time()
 A = []
+for j in range(0,n):
+    print("Pass",j)
+    for i in range(0,1000000):
+        A.append(random.randrange(0,1000000))
 
-for i in range(0,1000000):
-    A.append(random.randrange(0,1000000))
-
-A.sort()
+    A.sort()
+    A = []
 end = time.time()
 
 print(end - start)
